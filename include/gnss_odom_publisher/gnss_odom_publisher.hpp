71ea5e22cec468c0216ad6b0e22cd19a56d43d6c
YuukiNonaka <guitiankouki@gmail.com>
2023-09-19 17:46:31 +0900

    pub/subテスト実装

--- include/gnss_odom_publisher/gnss_odom_publisher.hpp
+++ include/gnss_odom_publisher/gnss_odom_publisher.hpp
@@ -19,7 +19,10 @@ using std::placeholders::_1;
 class GnssOdomPublisher : public rclcpp::Node
 {
     private:
+        rclcpp::Time subscribe_time;    // 一旦仮
+
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
 
         void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
 
--- src/gnss_odom_publisher.cpp
+++ src/gnss_odom_publisher.cpp
@@ -7,20 +7,46 @@
 GnssOdomPublisher::GnssOdomPublisher()
 : Node("gnss_odom_publisher")
 {
+  // read parameters
+
+
+  // pub/sub initialize
+  odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
   odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
     "odometry/gps/raw", 10, std::bind(&GnssOdomPublisher::odom_callback, this, _1));
-
-  // read parameters
 }
 
-void GnssOdomPublisher::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+void GnssOdomPublisher::odom_callback(const nav_msgs::msg::Odometry::SharedPtr odom)
 {
-  RCLCPP_INFO(this->get_logger(), "Position-> x: [%f]", msg->pose.pose.position.x);
+  //RCLCPP_INFO(this->get_logger(), "Position-> x: [%f]", odom->pose.pose.position.x);
+  subscribe_time = this -> get_clock() -> now();
+  publish();
 }
 
 void GnssOdomPublisher::publish()
 {
-  return;
+  nav_msgs::msg::Odometry odom;
+
+  /* 仮で適当な値をpublishさせる */
+
+  odom.header.stamp = subscribe_time;
+  odom.header.frame_id = std::string("odom_frame");
+  odom.child_frame_id = std::string("odom_child_frame");
+  // set the position
+  odom.pose.pose.position.x = 0.0;
+  odom.pose.pose.position.y = 0.0;
+  odom.pose.pose.position.z = 0.0;
+  odom.pose.pose.orientation.x = 0.0;
+  odom.pose.pose.orientation.y = 0.0;
+  odom.pose.pose.orientation.z = 0.0;
+  odom.pose.pose.orientation.w = 0.0;
+
+  // set the velocity
+  odom.twist.twist.linear.x = 0.0;
+  odom.twist.twist.linear.y = 0.0;
+  odom.twist.twist.angular.z = 0.0;
+
+  odom_pub_ -> publish(odom);
 }
 
 
