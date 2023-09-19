610806531266c746f7a8ddaaa08985feff585cac
YuukiNonaka <guitiankouki@gmail.com>
2023-09-19 16:50:09 +0900

    ヘッダーファイル分割、トピックのsubscribe部分実装

--- src/gnss_odom_publisher.cpp
+++ src/gnss_odom_publisher.cpp
@@ -22,3 +22,28 @@ void GnssOdomPublisher::publish()
 {
   return;
 }
+
+
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  std::shared_ptr<GnssOdomPublisher> node = std::make_shared<GnssOdomPublisher>();
+
+  try
+  {
+    while(rclcpp::ok())
+    {
+      rclcpp::spin_some(node);
+    }
+    rclcpp::shutdown();
+  }
+  catch(const rclcpp::exceptions::RCLError &e)
+  {
+    RCLCPP_ERROR(node->get_logger(), "unexpectedly failed with %s ", e.what());
+    rclcpp::shutdown();
+  }
+
+  return 0;
+}
